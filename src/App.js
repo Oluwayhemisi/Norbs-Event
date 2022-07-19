ea9f17c2415ec3edc10b83b2e215fdb850d8ad43
oluwayemisiismail <ismailoluwayemisi@gmail.com>
2022-07-19 17:01:53 +0100

    norbs event

---
+++ src/App.js
@@ -0,0 +1,14 @@
+
+import "./App.css";
+import Login from "./views/authentication/Login"
+
+function App () {
+  return (
+    <div className="App">
+      <Login/>
+     
+    </div>
+  )
+}
+
+export default App;
